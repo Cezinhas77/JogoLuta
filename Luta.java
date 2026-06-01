b16dde9fe4a0833892f1a81a61a5f68e8c6d0178
Cezinhas77 <caiocesaroliveira@alunos.utfpr.edu.br>
2026-06-01 13:59:31 -0300

    Refactor Luta class formatting and spacingLuta

--- Luta.java
+++ Luta.java
@@ -11,18 +11,18 @@ public class Luta {
 
     public void desferirSoco(Personagem executor, Personagem vitima){
         vitima.setVida(vitima.getVida() - executor.getSocoHP());
-        System.out.println(vitima.getNome() + "Perdeu: " + executor.getSocoHP() + "HP");
+        System.out.println(vitima.getNome() + " Perdeu: " + executor.getSocoHP() + "HP");
     }
 
     public void desferirChute(Personagem executor, Personagem vitima){
         vitima.setVida(vitima.getVida() - executor.getChuteHP());
-        System.out.println(vitima.getNome() + "Perdeu: " + executor.getChuteHP() + "HP");
+        System.out.println(vitima.getNome() + " Perdeu: " + executor.getChuteHP() + "HP");
 
     }
 
     public void desferirEspecial(Personagem executor, Personagem vitima){
         vitima.setVida(vitima.getVida() - executor.getEspecialHP());
-        System.out.println(vitima.getNome() + "Perdeu: " + executor.getEspecialHP() + "HP");
+        System.out.println(vitima.getNome() + " Perdeu: " + executor.getEspecialHP() + "HP");
 
     }
 
@@ -31,7 +31,7 @@ public class Luta {
     }
 
     public void exibirStatus(Personagem vitima){
-        System.out.println(vitima.getNome() + "Vida: " + vitima.getVida());
+        System.out.println(vitima.getNome() + " Vida: " + vitima.getVida());
     }
 
     public String getCenario(){
